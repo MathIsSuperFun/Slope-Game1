d9e0fc1fce41ea0a5853bcbf099d32b66c22e60c
MathIsSuperFun <161675744+MathIsSuperFun@users.noreply.github.com>
2024-03-07 18:25:57 -0500

    Update UnityProgress.js

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -18,7 +18,7 @@ function UnityProgress(gameInstance, progress) {
     gameInstance.container.appendChild(gameInstance.progress);
   }
   gameInstance.progress.full.style.width = (100 * progress) + "%";
-  gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
+  gameInstance.progress.empty.style.width = (100 * (100 - progress)) + "%";
+  if (progress == 100)
     gameInstance.logo.style.display = gameInstance.progress.style.display = "none";
 }
